ef31007ad765c7d913fb5d7bc643925be4c65558
George <98597244+GeorgeKontsevik@users.noreply.github.com>
2024-06-17 18:41:48 +0300

    fix/ api

--- app/api/routers/router_get_metric.py
+++ app/api/routers/router_get_metric.py
@@ -5,6 +5,8 @@ import shapely
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import HTMLResponse
 
+
+
 from contextlib import suppress
 
 from app.scripts.calculate_metric import calculate_tf_d1
@@ -13,49 +15,58 @@ from app.exceptions.exceptions import OutOfBoundaryError
 from .router_utils import load_gdf, load_graphml, load_main_city_center_node, load_region, load_country, get_region_roads_g, general_crs
 from transport_frames.src.metrics import indicators
 
+
+
+import geopandas as gpd
+from fastapi import Depends
+from app.api.schemes.schemes import Region
+import json
+import pathlib
+import os
+
 router = APIRouter()
 
-spb_center_osmid = 27490597
-lo_center_osmid = 27025179
 
-points = load_gdf('points')
-polys18 = load_gdf('polygons18')
-polys188 = load_gdf('polygons188')
-centers18 = load_gdf('17(18)_centers')
-admin_centers_LO_188_nodes = load_gdf('admin_centers_LO_188_nodes')
-svetlogorskoe = load_gdf('project Светогорского поселения')
-shlisserburg = load_gdf('project Шлиссельбург')
-pulkovo = load_gdf('pulkovo')
-aero_lodeynoe = load_gdf('Аэродром лодейнопольское поселение')
-aero_siversk = load_gdf('Аэродром Сиверск ')
-vodomy = load_gdf('Водомы итог (конец)')
-zhd_stops = load_gdf('ЖД остановки')
-oopt_lo = load_gdf('ООПТ ЛО')
+def read_json_file(filename):
+    with open(filename, 'r') as f:
+        json_object = json.load(f)
 
-fuel = load_gdf('fuel')
-aero_local = load_gdf('airports_local_LO')
-water_transport = load_gdf('water_transport_LO')
-international_aero = load_gdf('pulkovo').iloc[[0]]
-international_aero['geometry'] = shapely.centroid(international_aero.geometry).iloc[[0]]
+    result_dict = {}
 
-city_center = load_main_city_center_node(spb_center_osmid)
-lo_center = load_main_city_center_node(lo_center_osmid)
+    for key in json_object.keys():
+        if isinstance(json_object[key], str):  # check if the value is a string
+            try:
+                gdf = gpd.read_file(json_object[key])
+                result_dict[key] = gdf
+            except:
+                try:
+                    result_dict[key] = json.loads(json_object[key])
+                except json.JSONDecodeError:
+                    result_dict[key] = json_object[key]
+        else:
+            result_dict[key] = json_object[key]
 
-region_poly = load_region()
-country_poly = load_country()
+    return result_dict
 
-regions_of_russia = load_gdf('regions_of_russia')
-regions_of_russia = regions_of_russia[regions_of_russia['ISO3166-2']!='RU-CHU']
-regions_of_russia = regions_of_russia.to_crs(general_crs)
+# /Users/test/Documents/code/tf_api/app/db/indicators/Ленинградская_область/Ленинградская область_indicators_gpsp.json
 
-inter_g = load_graphml('inter')
-inter_g = indicators.prepare_graph(inter_g)
-region_roads_g = get_region_roads_g(region_poly, country_poly)
+def read_region_file(region_name, indicator_name:str):
+    cwd = os.getcwd()
+    parents = pathlib.Path(cwd)
+    path = str(parents) + f'/app/db/indicators/{region_name}/{region_name}_indicators_{indicator_name}.json'
+    file = read_json_file(path)
+    return file[list(file)[0]].to_json()
 
+
+@router.get("/get_gpsp/")
+def get_d0(query_params: Region=Depends()):   
+    return read_region_file(query_params.region.value, "gpsp")
     
-@router.post("/get_isochrone/")
-def get_d1():   
+@router.get("/get_mr/")
+def get_d1(query_params: Region=Depends()):   
+    return read_region_file(query_params.region.value, "mr")
 
-    d1 = calculate_tf_d1(region_roads_g, region_poly, polys18, points, polys188, inter_g, city_center, lo_center,fuel, zhd_stops, aero_local, aero_local, water_transport)
-
-    return d1
+@router.get("/get_region/")
+def get_d2(query_params: Region=Depends()):   
+    return read_region_file(query_params.region.value, "region")
+    
--- app/api/schemes/schemes.py
+++ app/api/schemes/schemes.py
@@ -1,16 +1,8 @@
-# from dongraphio import GraphType
+from app.api.schemes.enums import AvailableRegions
+from pydantic import BaseModel, Field
+from typing import Optional
 
-# from pydantic import BaseModel, Field
-# from typing import Optional
-
-# class Metric(BaseModel):
-#     """Base class for provision schema."""
-
-#     city_name: str = Field(..., example="krasnodar", max_length=20, min_length=1)
-#     graph_type: str = Field(..., example='public_transport')
-#     x: float = Field(..., example=45.043423, ge=0.0, le=180.0)
-#     y: float = Field(..., example=39.027212, ge=-90.0, le=90.0)
-#     weight_value: int = Field(..., example=10, gt=0)
-#     weight_type: str = Field(..., example='time_min')
-#     routes: bool = Field(..., example=False)
-#     # response_format: str = Field(..., example='html')
+class Region(BaseModel):
+    """Base class for provision schema."""
+    
+    region: AvailableRegions
