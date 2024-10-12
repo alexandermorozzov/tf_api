f877b5f243809069ef8274f79801d7efe6ad7afd
Alexander Morozov <alexandermorozzov@gmail.com>
2024-10-12 18:56:37 +0300

    update router_get_matrix

--- app/api/routers/router_get_matrix.py
+++ app/api/routers/router_get_matrix.py
@@ -13,27 +13,6 @@ class AccessibilityMatrixModel(BaseModel):
     columns: list[int]
     values: list[list[float]]
 
-# def load_matrix(region_id: int):
-#     matrix_path = os.path.join(DATA_PATH, f'matrices/{region_id}_car_matrix.pickle')
-    
-#     try:
-#         with open(matrix_path, 'rb') as f:
-#             matrix = pickle.load(f)
-#         return matrix
-#     except FileNotFoundError:
-#         raise HTTPException(status_code=404, detail="Matrix file not found")
-
-# @router.get('/{region_id}/get_matrix', response_model=AccessibilityMatrixModel)
-# def get_accessibility_matrix(region_id: int) -> AccessibilityMatrixModel:
-#     matrix = load_matrix(region_id)
-    
-#     res = {
-#         'index': matrix.index,
-#         'columns': matrix.columns,
-#         'values': matrix.values
-#     }
-
-#     return res
 
 def load_matrix(region_id: int, matrix_type: Literal['car', 'inter']):
     matrix_path = os.path.join(DATA_PATH, f'matrices/{region_id}_{matrix_type}_matrix.pickle')
