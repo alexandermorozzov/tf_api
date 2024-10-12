af24566abc1ca4c85c2cdebf6c1ba2a84022388d
Alexander Morozov <alexandermorozzov@gmail.com>
2024-10-12 18:55:58 +0300

    update router_get_matrix

--- app/api/routers/router_get_matrix.py
+++ app/api/routers/router_get_matrix.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel, Field
+from typing import Literal
 import os
 import geopandas as gpd
 import pickle
@@ -12,19 +13,41 @@ class AccessibilityMatrixModel(BaseModel):
     columns: list[int]
     values: list[list[float]]
 
-def load_matrix(region_id: int):
-    matrix_path = os.path.join(DATA_PATH, f'matrices/{region_id}_car_matrix.pickle')
+# def load_matrix(region_id: int):
+#     matrix_path = os.path.join(DATA_PATH, f'matrices/{region_id}_car_matrix.pickle')
+    
+#     try:
+#         with open(matrix_path, 'rb') as f:
+#             matrix = pickle.load(f)
+#         return matrix
+#     except FileNotFoundError:
+#         raise HTTPException(status_code=404, detail="Matrix file not found")
+
+# @router.get('/{region_id}/get_matrix', response_model=AccessibilityMatrixModel)
+# def get_accessibility_matrix(region_id: int) -> AccessibilityMatrixModel:
+#     matrix = load_matrix(region_id)
+    
+#     res = {
+#         'index': matrix.index,
+#         'columns': matrix.columns,
+#         'values': matrix.values
+#     }
+
+#     return res
+
+def load_matrix(region_id: int, matrix_type: Literal['car', 'inter']):
+    matrix_path = os.path.join(DATA_PATH, f'matrices/{region_id}_{matrix_type}_matrix.pickle')
     
     try:
         with open(matrix_path, 'rb') as f:
             matrix = pickle.load(f)
         return matrix
     except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="Matrix file not found")
+        raise HTTPException(status_code=404, detail=f"{matrix_type.capitalize()} matrix file not found for region {region_id}")
 
-@router.get('/{region_id}/get_matrix', response_model=AccessibilityMatrixModel)
-def get_accessibility_matrix(region_id: int) -> AccessibilityMatrixModel:
-    matrix = load_matrix(region_id)
+@router.get('/{region_id}/{matrix_type}/get_matrix', response_model=AccessibilityMatrixModel)
+def get_accessibility_matrix(region_id: int, matrix_type: Literal['car', 'inter']) -> AccessibilityMatrixModel:
+    matrix = load_matrix(region_id, matrix_type)
     
     res = {
         'index': matrix.index,
--- app/api/utils/get_matrix.py
+++ app/api/utils/get_matrix.py
@@ -8,19 +8,19 @@ from app.api.utils.constants import REGIONS_DICT, REGIONS_CRS, DATA_PATH
 from idu_clients import UrbanAPI
 from transport_frames.indicators.utils import availability_matrix
 
-def load_graph(region_id: int):
-    graph_file = os.path.join(DATA_PATH, f'graphs/{region_id}_car_graph.pickle')
+def load_graph(region_id: int, graph_type: str):
+    graph_file = os.path.join(DATA_PATH, f'graphs/{region_id}_{graph_type}_graph.pickle')
     if not os.path.exists(graph_file):
         region_name = REGIONS_DICT.get(region_id, f"Region ID {region_id}")
-        raise FileNotFoundError(f"Graph for {region_name} not found")
+        raise FileNotFoundError(f"{graph_type.capitalize()} graph for {region_name} not found.")
     
     with open(graph_file, "rb") as f:
         graph = pickle.load(f)
     
     return graph
 
-def check_matrix_exists(region_id: int):
-    matrix_file = os.path.join(DATA_PATH, f'matrices/{region_id}_car_matrix.pickle')
+def check_matrix_exists(region_id: int, matrix_type: str):
+    matrix_file = os.path.join(DATA_PATH, f'matrices/{region_id}_{matrix_type}_matrix.pickle')
     return os.path.exists(matrix_file), matrix_file
 
 async def load_settlement_points(region_id: int) -> gpd.GeoDataFrame:
@@ -40,25 +40,37 @@ def to_pickle(data, file_path: str) -> None:
     with open(file_path, "wb") as f:
         pickle.dump(data, f)
 
-def calculate_accessibility_matrix(graph, points, local_crs, region_id):
+def calculate_accessibility_matrix(graph, points, local_crs, region_id, matrix_type):
     try:
         acc_mx = availability_matrix(graph, points, points, local_crs=local_crs)
         return acc_mx
     except Exception as e:
         region_name = REGIONS_DICT.get(region_id, f"Region ID {region_id}")
-        raise RuntimeError(f"Error calculating the matrix for region {region_name}: {str(e)}")
-    
+        raise RuntimeError(f"Error calculating the {matrix_type} matrix for region {region_name}: {str(e)}")
+
 async def process_matrix():
     for region_id, region_name in REGIONS_DICT.items():
-        print(f"Load graph for {region_name}...")
-        graph = load_graph(region_id)
-        matrix_exists, matrix_file = check_matrix_exists(region_id)
-        if matrix_exists:
-            print(f"Matrix for {region_name} already exists")
-            continue 
-        print(f"Matrix for {region_name} not found. Creating...")
         local_crs = REGIONS_CRS[region_id]
-        points = await load_settlement_points(region_id)
-        acc_mx = calculate_accessibility_matrix(graph.graph, points, local_crs, region_id)
-        to_pickle(acc_mx, matrix_file)
-        print(f'Matrix for {region_name} has been successfully created')
+        print(f"Load car graph for {region_name}...")
+        car_graph = load_graph(region_id, 'car')
+        car_matrix_exists, car_matrix_file = check_matrix_exists(region_id, 'car')
+        if car_matrix_exists:
+            print(f"Car matrix for {region_name} already exists.")
+        else:
+            print(f"Car matrix for {region_name} not found. Creating...")
+            points = await load_settlement_points(region_id)
+            car_acc_mx = calculate_accessibility_matrix(car_graph.graph, points, local_crs, region_id, 'car')
+            to_pickle(car_acc_mx, car_matrix_file)
+            print(f'Car matrix for {region_name} has been successfully created.')
+
+        print(f"Load intermodal graph for {region_name}...")
+        inter_graph = load_graph(region_id, 'inter')
+        inter_matrix_exists, inter_matrix_file = check_matrix_exists(region_id, 'inter')
+        if inter_matrix_exists:
+            print(f"Intermodal matrix for {region_name} already exists.")
+        else:
+            print(f"Intermodal matrix for {region_name} not found. Creating...")
+            points = await load_settlement_points(region_id)
+            inter_acc_mx = calculate_accessibility_matrix(inter_graph, points, local_crs, region_id, 'inter')
+            to_pickle(inter_acc_mx, inter_matrix_file)
+            print(f'Intermodal matrix for {region_name} has been successfully created.')
